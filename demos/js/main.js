fbfe56af12a240d6c7248a47de0f0a1b290f83e6
Rhys Lloyd <justrhysism@gmail.com>
2012-12-06 18:08:58 +1030

    Nightly Commit

--- demos/js/main.js
+++ demos/js/main.js
@@ -1,1 +1,7 @@
-
+$(document).ready(function () {
+	/* Grid Switch */
+	$('#overlay-switch').click(function (event) {
+		$(this).toggleClass('off');
+		$('#overlay [class*="span"]').toggle(145);
+	});
+});
